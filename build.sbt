753c66fee7344a12c6d65616f08bc93045e85b1a
tenmakamatani <tenmakamatani0@gmail.com>
2019-06-13 09:36:28 +0900

    [update]完成

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala-grammer"
+
+version := "0.1"
+
+scalaVersion := "2.13.0"
---
+++ src/main/scala/ClassStudy.scala
@@ -0,0 +1,8 @@
+object ClassStudy extends App {
+  var input = ""
+  while (input != "tenma") {
+    input = scala.io.StdIn.readLine("お名前は？")
+    println(s"${input}？殺すぞ")
+  }
+  println("ああ、tenmaやったらええわ")
+}
